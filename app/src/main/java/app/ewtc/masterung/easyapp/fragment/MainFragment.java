0c545770b012757cb9c0bfb8ebe8a776ffdb500f
MasterUNG <phrombutr@gmail.com>
2017-10-08 10:20:47 +0700

    Start 8oct

--- app/src/main/java/app/ewtc/masterung/easyapp/fragment/MainFragment.java
+++ app/src/main/java/app/ewtc/masterung/easyapp/fragment/MainFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import app.ewtc.masterung.easyapp.R;
 
@@ -15,10 +17,51 @@ import app.ewtc.masterung.easyapp.R;
 
 public class MainFragment extends Fragment{
 
+    //Explicit
+    private ImageView imageView;
+    private Button decButton, incButton;
+
+    //My Constant
+    private int[] imageInts = new int[]{
+            R.drawable.icon1,
+            R.drawable.icon2,
+            R.drawable.icon3};
+    private int indexAnInt = 0;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Initial View
+        initialView();
+
+//        Increase Controller
+        increaseController();
+
+    }
+
+    private void increaseController() {
+        incButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+            }   // onClick
+        });
+    }
+
+    private void initialView() {
+
+        imageView = getView().findViewById(R.id.imvImage);
+        decButton = getView().findViewById(R.id.btnDecrease);
+        incButton = getView().findViewById(R.id.btnIncrease);
     }
 }   // Main Class
